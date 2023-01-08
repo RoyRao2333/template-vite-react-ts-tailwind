73eb0a394c447b02c9496758a39fea3489493c24
Roy <royrao2333@gmail.com>
2023-01-08 20:53:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
+import path from 'path';
 
 export default defineConfig({
   plugins: [eslintPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
