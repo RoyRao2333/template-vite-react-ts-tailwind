8fa5395f35c3c03f156d12fa7eb771e0f2b2f497
Roy <royrao2333@gmail.com>
2023-03-08 11:49:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [eslintPlugin()],
+  plugins: [react(), eslintPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
